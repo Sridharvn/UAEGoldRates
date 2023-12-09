1d4453bc813b6f34c49cf7803d2a5c86ad2b9726
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2023-12-09 15:45:07 +0530

    Added needed files

---
+++ lib/screens/chart_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:uae_gold_rates/services/data_handler_service.dart';
+
+class ChartScreen extends StatelessWidget {
+  const ChartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const Text("Hello"));
+  }
+}
---
+++ lib/services/data_handler_service.dart
@@ -0,0 +1,4 @@
+
+
+
+String csv_file_url="https://www.keralagold.com/mobile/dubai-gold-rate.csv";
