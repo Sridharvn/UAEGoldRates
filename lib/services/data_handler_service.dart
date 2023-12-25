03be839830c4be045b10b84df9272a62095cbdf8
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2023-12-25 23:46:40 +0530

    CSV recieved

--- lib/services/data_handler_service.dart
+++ lib/services/data_handler_service.dart
@@ -1,14 +1,16 @@
 import 'package:http/http.dart' as http;
+import './../utils/constants.dart' as constants;
 
 class CsvService {
   Future<List<Map<String, dynamic>>> fetchPriceRates() async {
     try {
-      final response = await http.get(Uri.parse('/* Your CSV URL */'));
+      final response = await http.get(Uri.parse(constants.csvLocation));
       if (response.statusCode == 200) {
         // Parse CSV data and extract price rates
         // Implement CSV parsing logic here
         // Return a list of Map or a custom model containing the price rates
-        return []/* List of extracted price rates */;
+        print(response.body);
+        return [];/* List of extracted price rates */;
       } else {
         throw Exception('Failed to fetch data');
       }
