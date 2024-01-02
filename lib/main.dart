9b8cee0d9f1888bc3f2d9d5aaacc4c2f47ddbe03
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2024-01-02 21:19:54 +0530

    Appbar color added in darkmode

--- lib/main.dart
+++ lib/main.dart
@@ -20,9 +20,9 @@ class MyApp extends StatelessWidget {
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       //   useMaterial3: true,
       // ),
-      theme: FlexThemeData.light(scheme: FlexScheme.green,appBarBackground: FlexColor.greenLightPrimary),
+      theme: FlexThemeData.light(scheme: FlexScheme.green),
         // The Mandy red, dark theme.
-        darkTheme: FlexThemeData.dark(scheme: FlexScheme.green),
+        darkTheme: FlexThemeData.dark(scheme: FlexScheme.green,appBarBackground: FlexColor.greenLightPrimary),
         // Use dark or light theme based on system setting.
         themeMode: ThemeMode.system,
       home:  TableScreen(),
